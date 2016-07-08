82992587f7c8877994326392fc06d03b5a278e81
jpa6227 <jpa6227@caelum133-11>
2016-07-07 22:43:56 -0300

    Configurando o agendador

--- src/br/com/caelum/financas/service/Agendador.java
+++ src/br/com/caelum/financas/service/Agendador.java
@@ -1,10 +1,29 @@
 package br.com.caelum.financas.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Stateless;
+import javax.inject.Singleton;
+
+@Singleton
 public class Agendador {
 
 	private static int totalCriado;
 
+	@PostConstruct
+	void posContrucao(){
+		System.out.println("criando o agendador");
+		totalCriado++;
+	}
+	
+	@PreDestroy
+	void preDestruicao(){
+		System.out.println("destruindo o agendador");
+	}
+	
+	
 	public void executa() {
+		
 		System.out.printf("%d instancias criadas %n", totalCriado);
 
 		// simulando demora de 4s na execucao
