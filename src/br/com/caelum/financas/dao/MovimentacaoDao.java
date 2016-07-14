6cae81abc44238dd83e2b78cafced0c8700682fc
jpa6227 <jpa6227@caelum133-11>
2016-07-13 21:52:16 -0300

    Consulta com Strings em criteria.

--- src/br/com/caelum/financas/dao/MovimentacaoDao.java
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -1,6 +1,7 @@
 package br.com.caelum.financas.dao;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -10,6 +11,8 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import br.com.caelum.financas.exception.ValorInvalidoException;
@@ -25,6 +28,38 @@ public class MovimentacaoDao {
 	// @PersistenceContext
 	EntityManager manager;
 
+	public List<Movimentacao> pesquisa(Conta conta, TipoMovimentacao tipoMovimentacao, Integer mes){
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Movimentacao> criteria = 
+				builder.createQuery(Movimentacao.class);
+		Root<Movimentacao> m = criteria.from(Movimentacao.class);
+		
+		Predicate conjunction = builder.conjunction();
+		if (conta.getId() != null) {
+			conjunction = 
+					builder.and(conjunction,
+							builder.equal(m.<Conta>get("conta"), conta));
+		}
+	
+		if (mes != null && mes != 0) {
+			Expression<Integer> expression =
+					builder.function("month", Integer.class, m.<Calendar> get("data"));
+			conjunction =
+					builder.and(conjunction, builder.equal(expression, mes));
+		}
+	
+		if (tipoMovimentacao != null) {
+			conjunction =
+					builder.and(conjunction,
+							builder.equal(
+									m.<TipoMovimentacao>get("tipoMovimentacao"),
+																			tipoMovimentacao));
+		}
+		
+		criteria.where(conjunction);
+		return manager.createQuery(criteria).getResultList();
+	}
+	
 	public BigDecimal somaMovimentacoesDoTitular(String titular){
 		
 		CriteriaBuilder builder = this.manager.getCriteriaBuilder();
