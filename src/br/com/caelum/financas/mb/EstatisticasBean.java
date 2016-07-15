52c9fab3bf32324abb3d8ef8a850061232f40fad
jpa6227 <jpa6227@caelum133-11>
2016-07-14 22:54:06 -0300

    Implementando estatísticas.

--- src/br/com/caelum/financas/mb/EstatisticasBean.java
+++ src/br/com/caelum/financas/mb/EstatisticasBean.java
@@ -1,13 +1,33 @@
 package br.com.caelum.financas.mb;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.stat.Statistics;
 
 @Named
 @ApplicationScoped
 public class EstatisticasBean {
 
+	@Inject
+	private EntityManager manager;
+	
+	private Statistics estatisticas;
+	
+		public Statistics getEstatisticas() {
+		return estatisticas;
+	}
+
+	public void setEstatisticas(Statistics estatisticas) {
+		this.estatisticas = estatisticas;
+	}
+
 		public void gera() {
-			System.out.println("Gerando estàtisitcas");
+			Session session = this.manager.unwrap(Session.class);
+			this.estatisticas = session.getSessionFactory().getStatistics();
+			System.out.println("Gerando estï¿½tisitcas");
 		}
 }
